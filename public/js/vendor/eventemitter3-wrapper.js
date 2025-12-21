f44b066d3e17a88180f671e59c3da68a4f168eeb
Andrei <official@dfn.wtf>
2025-12-21 07:21:42 -0600

    Fix eventemitter shim

--- public/js/vendor/eventemitter3-wrapper.js
+++ public/js/vendor/eventemitter3-wrapper.js
@@ -1,11 +1,11 @@
-import EventEmitter from 'https://cdn.jsdelivr.net/npm/eventemitter3@4.0.7/+esm';
+import _EventEmitter from 'https://cdn.jsdelivr.net/npm/eventemitter3@4.0.7/+esm';
+
+const emitter = _EventEmitter;
+emitter.EventEmitter = emitter;
+emitter.default = emitter;
 
 console.info('[SOLINK] EventEmitter3 shim loaded:', import.meta.url);
 
-const emitter = EventEmitter;
-emitter.EventEmitter = EventEmitter;
-emitter.default = EventEmitter;
-
 export default emitter;
-export { EventEmitter };
+export { emitter as EventEmitter };
 
