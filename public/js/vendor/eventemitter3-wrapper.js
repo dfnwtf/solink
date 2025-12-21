488bd1173e9ba88749f26d688426c10dbe20fbf7
Andrei <official@dfn.wtf>
2025-12-21 07:08:08 -0600

    Log vendor modules

--- public/js/vendor/eventemitter3-wrapper.js
+++ public/js/vendor/eventemitter3-wrapper.js
@@ -1,5 +1,7 @@
 import EventEmitter from 'https://cdn.jsdelivr.net/npm/eventemitter3@4.0.7/+esm';
 
+console.info('[SOLINK] EventEmitter3 shim loaded:', import.meta.url);
+
 const emitter = EventEmitter;
 emitter.EventEmitter = EventEmitter;
 emitter.default = EventEmitter;
